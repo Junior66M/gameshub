d44812f6cecbd817cc297027471f30c1a2db4669
Junior <marinosjunior667@gmail.com>
2025-05-30 15:09:47 -0500

    Pregunta2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { ListaJuegosComponent } from './components/lista-juegos/lista-juegos.component';
 import { DetalleJuegoComponent } from './components/detalle-juego/detalle-juego.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'juegos', component: ListaJuegosComponent },
   { path: 'juego/:id', component: DetalleJuegoComponent },
   { path: 'categorias', component: CategoriasComponent },
+   {path : 'estadisitcas', component:EstadisticasComponent},
   { path: 'categoria/:id', component: ListaJuegosComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+
+  
 ];
